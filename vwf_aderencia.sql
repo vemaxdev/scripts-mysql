8a8ed1b4fad6498a0ab68cef7f5d54bfd6854e0b
eliel-duate <elieletraud@gmail.com>
2026-08-19 14:34:03 -0300

    Corrige duplicacao de linhas na view pbi.vwf_aderencia
    nf_entrada_tratador possui varias linhas por oc_linha (itens da nota),
    o que multiplicava o resultado via LEFT JOIN. Troca para EXISTS,
    que so verifica presenca sem duplicar a linha principal.

--- vwf_aderencia.sql
+++ vwf_aderencia.sql
@@ -9,7 +9,11 @@ SELECT
     VWU.centrotrabalho,
     CT.ct_embraer AS ct_embraer_ordem,
     IF(CONVERT(IPA.ct_origem, SIGNED) = CT.ct_embraer, 'ADERENTE', 'DISCORDANTE') AS status_aderencia,
-    IF(NFT.oc_linha IS NOT NULL, 'SIM', 'NAO') AS possui_nota_tratador
+    IF(EXISTS (
+        SELECT 1
+        FROM manufatura.nf_entrada_tratador NFT
+        WHERE NFT.oc_linha = CONCAT(IPA.po, '/', LPAD(IPA.linha, 5, '0'))
+    ), 'SIM', 'NAO') AS possui_nota_tratador
 FROM manufatura.item_poack_atual IPA
 LEFT JOIN manufatura.ordem_fabricacao ODF
     ON CONCAT(IPA.po, '/', LPAD(IPA.linha, 5, '0')) = ODF.oc_linha
@@ -22,6 +26,4 @@ LEFT JOIN manufatura.vw_ultima_data_fluxo VWU
     ON VWU.ordemfabricacao = ODF.`of`
 LEFT JOIN manufatura.centro_trabalho CT
     ON CT.ct = VWU.centrotrabalho
-LEFT JOIN manufatura.nf_entrada_tratador NFT
-    ON NFT.oc_linha = CONCAT(IPA.po, '/', LPAD(IPA.linha, 5, '0'))
 WHERE IPA.action = 'LIDO';
